c997d18d30e2d59d9435e1e22d9d509f03cc60f1
Neon <danielemmanuel257@gmail.com>
2026-09-03 12:38:11 +0100

    finished operators

--- operators.js
+++ operators.js
@@ -21,6 +21,39 @@ let modulus = x % y;
 const stringMessage = `Sum of x and y is ${sum}`;
 
 y %= 10;
-console.log(`y after incrementing: ${y}`);
+// console.log(`y after incrementing: ${y}`);
 
-// 
+// COMPARISON OPERATORS
+let a = 10;
+let b = 20;
+
+let objectA = {
+    "name": "alice"
+}
+
+let objectB = objectA;
+
+// console.log(`Is objectA equal to objectB? ${objectA === objectB}`);
+// console.log(`Is a equal to b? ${a === b}`);
+
+// console.log(`Is a greater than b? ${a > b}`);
+// console.log(`Is a less than b? ${a < b}`);
+// console.log(`Is a greater than or equal to b? ${a >= b}`);
+// console.log(`Is a less than or equal to b? ${a <= b}`);
+// console.log(`Is a equal to b? ${a === b}`);
+// console.log(`Is a not equal to b? ${a != b}`);
+
+// LOGICAL OPERATORS
+// let isTrue = true;
+// let isFalse = false;
+
+let isTrue = 10 > 5;
+let isFalse = 10 < 5;
+
+console.log(`Is isTrue and isFalse? ${isTrue && isFalse}`);
+console.log(`Is isTrue or isFalse? ${isTrue || isFalse}`);
+console.log(`Is isTrue not isFalse? ${!isTrue}`);
+
+// console.log(isTrue && isFalse);
+// console.log(isTrue || isFalse);
+// console.log(!isTrue);
