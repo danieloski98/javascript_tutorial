4475140d8103101cf4733546a18e13fb98cac1da
Neon <danielemmanuel257@gmail.com>
2026-09-03 12:24:12 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let age;
+age = 200;
+age = 190;
+age = 180;
+
+const PIE = 3.124;
+
+//console.log(age);
+
+// HELLO THERE
+/** * Variable Types in javascript
+ * STRING
+ * NUMBER
+ * BOOLEAN
+ * ARRAY
+ * OBJECT
+ * NULL
+ * UNDEFINED
+*/
+
+let name = "john doe";
+let float = 3.14;
+let int = 45;
+let gender = "male";
+let isMale = gender !== "male";
+let isFemal = true
+let isNotFemale = false
+
+let newVariable = null;
+
+let people = ["John", 123, true, [null]];
+
+console.log(people[1]);
+
+let person = {
+    "name": "John Doe",
+    "age": 30,
+    device: 'Nintendo Switch',
+}
+console.log(person);
---
+++ operators.js
@@ -0,0 +1,26 @@
+// MATHEMATICAL OPERATORS
+// ASSIGNMENT OPERATORS
+// COMPARISM OPERATORS
+// LOGICAL OPERATORS
+
+let x = 10
+let y = 3;
+
+let sum = x + y;
+let multiplication = x * y;
+let division = x / y;
+let subtraction = x - y;
+let modulus = x % y;
+
+// console.log(`SUM - ${sum}`);
+// console.log(`MULTIPLICATION - ${multiplication}`);
+// console.log(`DIVISION - ${division}`);
+// console.log(`SUBTRACTION - ${subtraction}`);
+// console.log(`MODULUS - ${modulus}`);
+
+const stringMessage = `Sum of x and y is ${sum}`;
+
+y %= 10;
+console.log(`y after incrementing: ${y}`);
+
+// 
